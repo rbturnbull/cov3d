1f801e797ff9e39ddfbefa221030688fafea848d
rturnbull <robert.turnbull@unimelb.edu.au>
2022-06-12 14:30:23 +1000

    :zap: code running on spartan

--- cov3d/transforms.py
+++ cov3d/transforms.py
@@ -11,18 +11,39 @@ class TensorBool(TensorBase):
 
 
 def read_ct_scans(path:Path):
-    slices = sorted(path.glob('*.jpg'), key=lambda x:int(x.stem))
+    slices = sorted([x for x in path.glob('*.jpg') if x.stem.isnumeric()], key=lambda x:int(x.stem))
     num_slices = len(slices)
     assert num_slices > 0
 
-    # Get resolution from first slice
-    with Image.open(slices[0]) as im:
-        size = im.size
+    max_slices = 64
+
+    if num_slices < max_slices:
+        factor = max_slices//num_slices
+        slices += slices * factor
+        slices = slices[:max_slices]
+        num_slices = len(slices)
+
+    if num_slices > max_slices:
+        start = (num_slices-max_slices)//2
+        slices = slices[start:start+max_slices]
+        num_slices = len(slices)
+    
+    size = (128,128)
+    if not size:
+        # Get resolution from first slice
+        with Image.open(slices[0]) as im:
+            size = im.size
+
+    if num_slices != max_slices:
+        raise ValueError(f"{num_slices} != {max_slices}")
 
     tensor = torch.zeros( (1, num_slices, size[1], size[0]) )
     for index, slice in enumerate(slices):
         with Image.open(slice) as im:
             im = im.convert('L')
+            if im.size != size:
+                im = im.resize(size,Image.BICUBIC)
+                # raise ValueError(f"The size of image {path} ({im.size}) is not consistent with the first image in this scan {size}")
             im_data = to_tensor(im)
             tensor[:,index,:,:] = im_data
 
@@ -30,7 +51,7 @@ def read_ct_scans(path:Path):
     
 
 def bool_to_tensor(input:bool):
-    return TensorBool([input]).float()
+    return torch.FloatTensor([input])
 
 
 def BoolBlock():

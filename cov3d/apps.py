9cc0cb0b1ba84ded7cf0bdd64033daceba5b430e
rturnbull <robert.turnbull@unimelb.edu.au>
2022-06-15 18:21:46 +1000

    :zap: adding models from torchvision video pretrained models

--- cov3d/apps.py
+++ cov3d/apps.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from torch import nn
 from fastai.data.core import DataLoaders
-from fastai.data.transforms import GrandparentSplitter, get_image_files
+from fastai.data.transforms import GrandparentSplitter, get_image_files, FuncSplitter
 from fastai.data.core import DataLoaders
 from fastai.data.block import DataBlock, CategoryBlock, TransformBlock
 from fastai.metrics import accuracy, Precision, Recall, F1Score
@@ -13,6 +13,8 @@ import fastapp as fa
 from rich.console import Console
 console = Console()
 from fastapp.metrics import logit_f1, logit_accuracy
+
+from torchvision.models import video
 
 from .transforms import CTScanBlock, BoolBlock, CTSliceBlock
 from .models import ResNet3d
@@ -37,11 +39,20 @@ class DictionaryGetter():
         return self.dictionary[key]
 
 
+def is_validation(scan_path:Path):
+    if not scan_path.is_dir():
+        scan_path = scan_path.parent
+    
+    return  scan_path.parent.parent.name.startswith("validation")
+
+
 class Cov3dCombinedGetter():
     def __init__(self, severity_dictionary):
         self.severity_dictionary = severity_dictionary
 
     def __call__(self, scan_path):
+        if not scan_path.is_dir():
+            scan_path = scan_path.parent
         parent_name = scan_path.parent.name
         if parent_name == "covid":
             has_covid = True
@@ -418,7 +429,8 @@ class Cov3dCombined(VisionApp):
             subdir_paths = [path for path in subdir.iterdir() if path.name.startswith("ct_scan")]
             if len(subdir_paths) == 0:
                 raise FileNotFoundError(f"Cannot file directories with prefix 'ct_scan' in {subdir}")
-            paths += subdir_paths
+            for scan_dir in subdir_paths:
+                paths += list(get_image_files(scan_dir))
 
         severity = dict()
 
@@ -435,7 +447,7 @@ class Cov3dCombined(VisionApp):
 
         datablock = DataBlock(
             blocks=(CTSliceBlock, TransformBlock),
-            splitter=GrandparentSplitter(train_name='train', valid_name='validation'),
+            splitter=FuncSplitter(is_validation),
             get_y=Cov3dCombinedGetter(severity),
         )
 
@@ -475,7 +487,7 @@ class Cov3dCombined(VisionApp):
         ]
 
     def monitor(self):
-        return "valid_loss"
+        return "presence_f1"
 
     def inference_dataloader(self, learner, **kwargs):
         self.inference_images = get_image_files(Path("../validation/non-covid/"))
@@ -501,3 +513,122 @@ class Cov3dCombined(VisionApp):
 
         console.print(f"Writing results for {len(results_df)} sequences to: {output_csv}")
         results_df.to_csv(output_csv)
+
+
+class Covideo(fa.FastApp):
+    """
+    A deep learning model to detect the presence and severity of COVID19 in patients from CT-scans.
+    """
+    def dataloaders(
+        self,
+        directory:Path = fa.Param(help="The data directory."),
+        batch_size:int = fa.Param(default=4, help="The batch size."),
+        training_csv:Path = fa.Param(help="The path to the training CSV file with severity information."),
+        validation_csv:Path = fa.Param(help="The path to the validation CSV file with severity information."),
+    ) -> DataLoaders:
+        """
+        Creates a FastAI DataLoaders object which Cov3d uses in training and prediction.
+
+        Args:
+            directory (Path): The data directory.
+            batch_size (int, optional): The number of elements to use in a batch for training and prediction. Defaults to 32.
+
+        Returns:
+            DataLoaders: The DataLoaders object.
+        """
+        directory = Path(directory).resolve()
+        paths = []
+
+        subdirs = ["train/covid", "train/non-covid", "validation/covid", "validation/non-covid"]
+        for s in subdirs:
+            subdir = directory/s
+            if not subdir.exists():
+                raise FileNotFoundError(f"Cannot find directory '{subdir}'.")
+            subdir_paths = [path for path in subdir.iterdir() if path.name.startswith("ct_scan")]
+            if len(subdir_paths) == 0:
+                raise FileNotFoundError(f"Cannot file directories with prefix 'ct_scan' in {subdir}")
+            paths += subdir_paths
+
+        severity = dict()
+
+        def read_severity_csv(csv:Path, dir:str):
+            df = pd.read_csv(csv, delimiter=";")
+            for _, row in df.iterrows():
+                path = directory/f"{dir}/covid"/row['Name']
+                if not path.exists():
+                    raise FileNotFoundError(f"Cannot find directory {path}")
+                severity[path] = row['Category']
+        
+        read_severity_csv(training_csv, dir="train")
+        read_severity_csv(validation_csv, dir="validation")
+
+        datablock = DataBlock(
+            blocks=(CTScanBlock, TransformBlock),
+            splitter=FuncSplitter(is_validation),
+            get_y=Cov3dCombinedGetter(severity),
+        )
+
+        dataloaders = DataLoaders.from_dblock(
+            datablock, 
+            source=paths,
+            bs=batch_size,
+        )
+        
+        dataloaders.c = 2
+        return dataloaders    
+
+    def model(
+        self,
+        model_name:str = "r2plus1d_18",
+        pretrained:bool = True,
+    ) -> nn.Module:
+        """
+        Creates a deep learning model for the Cov3d to use.
+
+        Returns:
+           nn.Module: The created model.
+        """ 
+        get_model = getattr(video, model_name)
+        model = get_model(pretrained=pretrained)
+        model.fc = nn.Linear(in_features=model.fc.in_features, out_features=2, bias=False)
+
+        return model
+
+    def loss_func(self):
+        return Cov3dLoss()
+
+    def metrics(self):
+        return [
+            PresenceF1(),
+            PresenceAccuracy(),
+            SeverityF1(),
+            SeverityAccuracy(),
+        ]
+
+    def monitor(self):
+        return "presence_f1"
+
+    # def inference_dataloader(self, learner, **kwargs):
+    #     self.inference_images = get_image_files(Path("../validation/non-covid/"))
+    #     # self.inference_images = list({x.parent for x in self.inference_images})
+    #     dataloader = learner.dls.test_dl(self.inference_images)
+    #     self.categories = ["presence", "severity"]
+    #     return dataloader
+
+    # def output_results(
+    #     self,
+    #     results,
+    #     output_csv: Path = fa.Param(default=None, help="A path to output the results as a CSV."),
+    #     **kwargs,
+    # ):
+    #     results_df = pd.DataFrame(results[0].numpy(), columns=self.categories)
+    #     results_df["image"] = self.inference_images
+    #     results_df["scan"] = [image.parent.name for image in self.inference_images]
+    #     predictions = torch.argmax(results[0], dim=1)
+    #     results_df['prediction'] = [self.categories[p] for p in predictions]
+
+    #     if not output_csv:
+    #         raise Exception("No output file given.")
+
+    #     console.print(f"Writing results for {len(results_df)} sequences to: {output_csv}")
+    #     results_df.to_csv(output_csv)

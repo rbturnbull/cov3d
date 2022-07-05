4417c5190866810e5eb0732ed0769f84ed0503a6
rturnbull <robert.turnbull@unimelb.edu.au>
2022-07-05 12:03:07 +1000

    :zap: adding more stuff to script to do validation

--- macrof1.py
+++ macrof1.py
@@ -2,11 +2,11 @@ import typer
 from pathlib import Path
 from sklearn.metrics import f1_score
 import pandas as pd
-from cov3d.apps import Covideo
+from cov3d.apps import Covideo, CovideoSeverity
 import wandb
 
-def main(model_dir:Path, mc_samples:int=10, log_wandb:bool = True):
-    app = Covideo()
+def main(model_dir:Path, mc_samples:int=0, log_wandb:bool = True, mc_dropout:bool=False, severity:bool=False):
+    app = Covideo() if not severity else CovideoSeverity()
     covid_results = app(
         pretrained=model_dir/"export.pkl",
         scan_dir="../validation/covid/",
@@ -14,25 +14,17 @@ def main(model_dir:Path, mc_samples:int=10, log_wandb:bool = True):
         # scan_dir=[], # hack
         scan=[],
         mc_samples=mc_samples,
+        output_csv=model_dir/"validation-covid.csv",
+        output_mc=model_dir/"validation-covid.pt",
         noncovid_txt=model_dir/"validation.fn.txt",
         covid_txt=model_dir/"validation.tp.txt",
         mild_txt=model_dir/"mild.txt",
         moderate_txt=model_dir/"moderate.txt",
         severe_txt=model_dir/"severe.txt",
         critical_txt=model_dir/"critical.txt",
+        mc_dropout=mc_dropout,
     )
     covid_results["true"] = 1
-    noncovid_results = app(
-        pretrained=model_dir/"export.pkl",
-        scan_dir="../validation/non-covid/",
-        # scan="../validation/non-covid/ct_scan_3",
-        # scan_dir=[], # hack
-        scan=[],
-        mc_samples=mc_samples,
-        noncovid_txt=model_dir/"validation.tn.txt",
-        covid_txt=model_dir/"validation.fp.txt",
-    )
-    noncovid_results["true"] = 0
 
     severity_df = pd.read_csv("../val_partition_covid_categories.csv", sep=";")
     severity_categories = ["mild", "moderate", "severe", "critical"]
@@ -40,7 +32,25 @@ def main(model_dir:Path, mc_samples:int=10, log_wandb:bool = True):
     for _, row in severity_df.iterrows():
         covid_results.loc[ covid_results["name"] == row['Name'], 'true_severity' ] = severity_categories[int(row['Category']) - 1]
 
-    results = pd.concat([covid_results,noncovid_results])
+    if not severity:
+        noncovid_results = app(
+            pretrained=model_dir/"export.pkl",
+            scan_dir="../validation/non-covid/",
+            output_csv=model_dir/"validation-noncovid.csv",
+            output_mc=model_dir/"validation-noncovid.pt",
+            # scan="../validation/non-covid/ct_scan_3",
+            # scan_dir=[], # hack
+            scan=[],
+            mc_samples=mc_samples,
+            noncovid_txt=model_dir/"validation.tn.txt",
+            covid_txt=model_dir/"validation.fp.txt",
+            mc_dropout=mc_dropout,
+        )
+        noncovid_results["true"] = 0
+        results = pd.concat([covid_results,noncovid_results])
+    else:
+        results = covid_results
+        
     macro_f1 = f1_score(results["true"], results["COVID19 positive"], average="macro")
     results.to_csv(model_dir/"validation.csv", index=False)
     print("macro_f1", macro_f1)

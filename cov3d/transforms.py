162b9fc71059eecec9b61558eff9b29be00afa92
rturnbull <robert.turnbull@unimelb.edu.au>
2023-03-18 12:31:32 +1100

    :zap: allowing it to read scan dirs which have missing images

--- cov3d/transforms.py
+++ cov3d/transforms.py
@@ -1,7 +1,7 @@
 from fastcore.transform import Transform
 from fastai.data.block import TransformBlock
 from pathlib import Path
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 import torch
 from fastai.torch_core import TensorBase
 import random
@@ -61,6 +61,8 @@ class ReadCTScanCrop(Transform):
         
         if tensor_path.exists():
             x = torch.load(str(tensor_path))
+            if x.dtype == torch.float64:
+                x = x.half()
             return x
 
         tensor_path.parent.mkdir(exist_ok=True, parents=True)
@@ -77,8 +79,12 @@ class ReadCTScanCrop(Transform):
             original_size = (512,512)
             data = np.zeros( (len(slices), original_size[1], original_size[0]), dtype=int )
             slices_to_keep = []
-            for i in range(len(slices)):
-                im = Image.open(path/f"{i}.jpg").convert('L')
+            for i, slice in enumerate(slices):
+                try:
+                    im = Image.open(slice).convert('L')
+                except UnidentifiedImageError:
+                    print(f"Cannot open {slice}")
+                    pass
                 if im.size != original_size:
                     im = im.resize(original_size, Image.BICUBIC)
                 im_data = np.asarray(im)
@@ -145,142 +151,157 @@ class ReadCTScanCrop(Transform):
                             max_result[i] = result
                             seed[i] = label_image==label_index
 
-            assert seed[0] is not None
-            assert seed[1] is not None
+            if seed[0] is not None and seed[1] is not None:
 
-            # Remove air connected to the boundary to the front, back, right and left (not head and feet)
-            def get_boundary(data, threshold=70, closing=True, erosion_radius=None, jstart=False, jend=False, kstart=False, kend=False, show=False):
-                binary_strict = data < threshold
-                if erosion_radius:
-                    binary_strict = binary_opening(binary_strict, ball(erosion_radius, decomposition="sequence"))
+                # Remove air connected to the boundary to the front, back, right and left (not head and feet)
+                def get_boundary(data, threshold=70, closing=True, erosion_radius=None, jstart=False, jend=False, kstart=False, kend=False, show=False):
+                    binary_strict = data < threshold
+                    if erosion_radius:
+                        binary_strict = binary_opening(binary_strict, ball(erosion_radius, decomposition="sequence"))
 
-                end_mask = np.ones_like(binary, dtype=bool)
-                end_mask[:,0,:] = jstart
-                end_mask[:,-1,:] = jend
-                end_mask[:,:,0] = kstart
-                end_mask[:,:,-1] = kend
-                boundary = clear_border(binary_strict, mask=end_mask) ^ binary_strict
-                if closing:
-                    boundary = binary_closing( boundary, ball(3, decomposition="sequence") )
+                    end_mask = np.ones_like(binary, dtype=bool)
+                    end_mask[:,0,:] = jstart
+                    end_mask[:,-1,:] = jend
+                    end_mask[:,:,0] = kstart
+                    end_mask[:,:,-1] = kend
+                    boundary = clear_border(binary_strict, mask=end_mask) ^ binary_strict
+                    if closing:
+                        boundary = binary_closing( boundary, ball(3, decomposition="sequence") )
 
-                if show:
-                    plot_volume(boundary, n_steps=3).show()
-                    sys.exit()
+                    if show:
+                        plot_volume(boundary, n_steps=3).show()
+                        sys.exit()
 
-                return boundary
-                
-            # Try to remove boundary if not in seed
-            removed_boundary = False
-            # Try to remove boundary if not in seed
-            removed_boundary = False
-            threshold = self.threshold
-            def remove_boundary(data, binary, threshold):
+                    return boundary
+                    
                 # Try to remove boundary if not in seed
-                total_boundary = np.zeros_like(binary, dtype=bool)
-                while threshold > 0:
-                    for erosion_radius in [0,5,10,15]:
-                        print(f"trying to clear boundary at threshold {threshold}, erosion {erosion_radius}")
+                removed_boundary = False
+                # Try to remove boundary if not in seed
+                removed_boundary = False
+                threshold = self.threshold
+                def remove_boundary(data, binary, threshold):
+                    # Try to remove boundary if not in seed
+                    total_boundary = np.zeros_like(binary, dtype=bool)
+                    while threshold > 0:
+                        for erosion_radius in [0,5,10,15]:
+                            print(f"trying to clear boundary at threshold {threshold}, erosion {erosion_radius}")
+                            
+                            boundary = get_boundary(data, threshold=threshold, jstart=False, jend=False, kstart=False, kend=False, erosion_radius=erosion_radius)
+                            if boundary[max_index[0], seed[0]].sum() == 0 and boundary[max_index[1], seed[1]].sum() == 0:
+                                print("clearing boundary")
+                                binary[boundary] = 0
+                                total_boundary = total_boundary | boundary
+                                break
                         
-                        boundary = get_boundary(data, threshold=threshold, jstart=False, jend=False, kstart=False, kend=False, erosion_radius=erosion_radius)
-                        if boundary[max_index[0], seed[0]].sum() == 0 and boundary[max_index[1], seed[1]].sum() == 0:
-                            print("clearing boundary")
-                            binary[boundary] = 0
-                            total_boundary = total_boundary | boundary
+                        if erosion_radius == 0:
                             break
-                    
-                    if erosion_radius == 0:
+                        
+                        threshold -= 10
+                    binary[total_boundary] = 0
+                    return binary
+
+                binary = remove_boundary(data, binary, threshold=threshold)
+
+                # Dilate a bit to fill in holes and to join the lungs if necessary
+                dilated = binary_dilation(binary, ball(3, decomposition="sequence"))
+
+                # label regions
+                label_image = label(dilated)
+
+                # find label with seed
+                seed_label = label_image[max_index[0], seed[0]]
+                assert seed_label.min() == seed_label.max()
+                seed_label_a = seed_label[0]
+
+                seed_label = label_image[max_index[1], seed[1]]
+                assert seed_label.min() == seed_label.max()
+                seed_label_b = seed_label[0]
+
+                lungs = (label_image == seed_label_a) | (label_image == seed_label_b)
+
+                # find bounds of segmented lungs
+                for start_i in range(lungs.shape[0]):
+                    if lungs[start_i,:,:].sum() > 0:
                         break
-                    
-                    threshold -= 10
-                binary[total_boundary] = 0
-                return binary
 
-            binary = remove_boundary(data, binary, threshold=threshold)
+                for end_i in reversed(range(lungs.shape[0])):
+                    if lungs[end_i,:,:].sum() > 0:
+                        break
 
-            # Dilate a bit to fill in holes and to join the lungs if necessary
-            dilated = binary_dilation(binary, ball(3, decomposition="sequence"))
+                for start_j in range(lungs.shape[1]):
+                    if lungs[:,start_j,:].sum() > 0:
+                        break
 
-            # label regions
-            label_image = label(dilated)
+                for end_j in reversed(range(lungs.shape[1])):
+                    if lungs[:,end_j,:].sum() > 0:
+                        break
 
-            # find label with seed
-            seed_label = label_image[max_index[0], seed[0]]
-            assert seed_label.min() == seed_label.max()
-            seed_label_a = seed_label[0]
+                for start_k in range(lungs.shape[2]):
+                    if lungs[:,:,start_k].sum() > 0:
+                        break
 
-            seed_label = label_image[max_index[1], seed[1]]
-            assert seed_label.min() == seed_label.max()
-            seed_label_b = seed_label[0]
+                for end_k in reversed(range(lungs.shape[2])):
+                    if lungs[:,:,end_k].sum() > 0:
+                        break
 
-            lungs = (label_image == seed_label_a) | (label_image == seed_label_b)
+                # crop original data according to the bounds of the segmented lungs
+                # also scale from zero to one
+                lungs_cropped = data[ start_i:end_i+1, start_j:end_j+1, start_k:end_k+1]/255.0
 
-            # find bounds of segmented lungs
-            for start_i in range(lungs.shape[0]):
-                if lungs[start_i,:,:].sum() > 0:
-                    break
+                crop_log = tensor_path.parent/f"{path.name}.crop.txt"
+                crop_log.write_text(f"{relative_dir},{slice_count},{start_i},{end_i},{start_j},{end_j},{start_k},{end_k},{data.size},{lungs_cropped.size},{lungs_cropped.size/data.size*100.0}\n")
 
-            for end_i in reversed(range(lungs.shape[0])):
-                if lungs[end_i,:,:].sum() > 0:
-                    break
+                # Save seeds
+                for i in range(2):
+                    rgb = np.repeat( np.expand_dims(data[max_index[i]], axis=2), 3, axis=2)
+                    rgb[start_j:end_j+1, start_k:end_k+1, 2 ] = 0
+                    rgb[seed[i], 2 ] = 0
+                    rgb[seed[i], 1 ] = 0
+                    im = Image.fromarray(rgb.astype(np.uint8))
+                    seed_dir = preprossed_dir / "seed" / relative_dir
+                    seed_dir.mkdir(exist_ok=True, parents=True)
+                    seed_path = seed_dir/f"{path.name}.seed-i-{i}.jpg"
+                    im.save(seed_path)    
 
-            for start_j in range(lungs.shape[1]):
-                if lungs[:,start_j,:].sum() > 0:
-                    break
+                    rgb = np.repeat( np.expand_dims(data[:,int(start_j/2+end_j/2),:], axis=2), 3, axis=2)
+                    rgb[start_i:end_i+1, start_k:end_k+1, 2 ] = 0
+                    rgb[max_index[i]-start_i, start_k:end_k+1, 1 ] = 0
+                    im = Image.fromarray(rgb.astype(np.uint8))
+                    seed_path = seed_dir/f"{path.name}.seed-j-{i}.jpg"
+                    im.save(seed_path)    
 
-            for end_j in reversed(range(lungs.shape[1])):
-                if lungs[:,end_j,:].sum() > 0:
-                    break
+                    rgb = np.repeat( np.expand_dims(data[:,:,int(start_k/4+end_k/4)], axis=2), 3, axis=2)
+                    rgb[start_i:end_i+1, start_j:end_j+1, 2 ] = 0
+                    rgb[max_index[i]-start_i, start_j:end_j+1, 1 ] = 0
+                    im = Image.fromarray(rgb.astype(np.uint8))
+                    seed_path = seed_dir/f"{path.name}.seed-k-{i}.jpg"
+                    im.save(seed_path)    
 
-            for start_k in range(lungs.shape[2]):
-                if lungs[:,:,start_k].sum() > 0:
-                    break
+                data = lungs_cropped            
 
-            for end_k in reversed(range(lungs.shape[2])):
-                if lungs[:,:,end_k].sum() > 0:
-                    break
-
-            # crop original data according to the bounds of the segmented lungs
-            # also scale from zero to one
-            lungs_cropped = data[ start_i:end_i+1, start_j:end_j+1, start_k:end_k+1]/255.0
-
-            crop_log = tensor_path.parent/f"{path.name}.crop.txt"
-            crop_log.write_text(f"{relative_dir},{slice_count},{start_i},{end_i},{start_j},{end_j},{start_k},{end_k},{data.size},{lungs_cropped.size},{lungs_cropped.size/data.size*100.0}\n")
-
-            # Save seeds
-            for i in range(2):
-                rgb = np.repeat( np.expand_dims(data[max_index[i]], axis=2), 3, axis=2)
-                rgb[start_j:end_j+1, start_k:end_k+1, 2 ] = 0
-                rgb[seed[i], 2 ] = 0
-                rgb[seed[i], 1 ] = 0
-                im = Image.fromarray(rgb.astype(np.uint8))
-                seed_dir = preprossed_dir / "seed" / relative_dir
-                seed_dir.mkdir(exist_ok=True, parents=True)
-                seed_path = seed_dir/f"{path.name}.seed-i-{i}.jpg"
-                im.save(seed_path)    
-
-                rgb = np.repeat( np.expand_dims(data[:,int(start_j/2+end_j/2),:], axis=2), 3, axis=2)
-                rgb[start_i:end_i+1, start_k:end_k+1, 2 ] = 0
-                rgb[max_index[i]-start_i, start_k:end_k+1, 1 ] = 0
-                im = Image.fromarray(rgb.astype(np.uint8))
-                seed_path = seed_dir/f"{path.name}.seed-j-{i}.jpg"
-                im.save(seed_path)    
-
-                rgb = np.repeat( np.expand_dims(data[:,:,int(start_k/4+end_k/4)], axis=2), 3, axis=2)
-                rgb[start_i:end_i+1, start_j:end_j+1, 2 ] = 0
-                rgb[max_index[i]-start_i, start_j:end_j+1, 1 ] = 0
-                im = Image.fromarray(rgb.astype(np.uint8))
-                seed_path = seed_dir/f"{path.name}.seed-k-{i}.jpg"
-                im.save(seed_path)    
-
-            data = lungs_cropped
-
-        data = resize(data, (self.depth,self.height,self.width), order=3)        
-
-        tensor = torch.unsqueeze(torch.as_tensor(data), dim=0)
+        data_resized = resize(data, (self.depth,self.height,self.width), order=3)        
+        tensor = torch.unsqueeze(torch.as_tensor(data_resized), dim=0)
         if self.fp16:
             tensor = tensor.half()
         print("save", str(tensor_path))
         torch.save(tensor, str(tensor_path))
+
+        # HACK
+        # HACK
+        # HACK
+        # HACK
+        # def write_image(depth, width, height):
+        #     preprossed_dir = root_dir/ f"{depth}x{height}x{width}{autocrop_str}{fp16_str}"
+        #     filename = f"{path.name}-{depth}x{height}x{width}.pt"
+        #     tensor_path = preprossed_dir / relative_dir / filename
+        #     tensor_path.parent.mkdir(exist_ok=True, parents=True)
+        #     data_resized = resize(data, (depth,height,width), order=3)        
+        #     tensor = torch.unsqueeze(torch.as_tensor(data_resized), dim=0)
+        #     torch.save(tensor, str(tensor_path))
+
+        # assert self.width == 320
+        # write_image(64, 128, 128)
+        # write_image(256, 256, 176)
 
         return tensor
 
